7ce1177c1813fa3cf6779c907a103fbd7eec6194
linchyde <lincoln@hydeglobalmedia.com>
2022-11-14 09:22:32 +1000

    Add project files.

---
+++ Services/FileUploads.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace FileUpload.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadFile(IBrowserFile file);
+    }
+    public class FileUploads : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUploads> _logger;
+
+        public FileUploads(IWebHostEnvironment webHostEnvironment, ILogger<FileUploads> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+        public async Task UploadFile(IBrowserFile file)
+        {
+            if(file is not null)
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.Name);
+
+                    using (var stream = file.OpenReadStream())
+                    {
+                        var fileStream = File.Create(uploadPath);
+                        await stream.CopyToAsync(fileStream);
+                        fileStream.Close();
+                    }
+
+
+                }catch(Exception ex) 
+                {
+                    _logger.LogError(ex.ToString());    
+                }
+            }
+        }
+    }
+}
