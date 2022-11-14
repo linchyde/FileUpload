d943df3eef18874b8d6d403e6a4370357a269417
linchyde <lincoln@hydeglobalmedia.com>
2022-11-14 10:04:55 +1000

    updated

--- Services/FileUploads.cs
+++ Services/FileUploads.cs
@@ -5,6 +5,7 @@ namespace FileUpload.Services
     public interface IFileUpload
     {
         Task UploadFile(IBrowserFile file);
+        Task<string> GeneratePreviewUrl(IBrowserFile file);
     }
     public class FileUploads : IFileUpload
     {
@@ -38,6 +39,24 @@ namespace FileUpload.Services
                     _logger.LogError(ex.ToString());    
                 }
             }
+        }
+
+      
+
+        public async Task<string> GeneratePreviewUrl(IBrowserFile file)
+        {
+            if(!file.ContentType.Contains("image"))
+            {
+                if(file.ContentType.Contains("pdf"))
+                {
+                    return "images/pdf_logo.png";
+                }
+            }
+
+            var resizedImage = await file.RequestImageFileAsync(file.ContentType, 100, 100);
+            var buffer = new byte[resizedImage.Size];
+            await resizedImage.OpenReadStream().ReadAsync(buffer);
+            return $"data:{file.ContentType};base64,{Convert.ToBase64String(buffer)}";
         }
     }
 }
